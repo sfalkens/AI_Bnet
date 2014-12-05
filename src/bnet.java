783af8a36aaaf0a1236b87a9780d0530284a1003
Sarah Falkenstein <sfalkens@mines.edu>
2014-12-04 19:29:39 -0700

    Progress so far

--- src/bnet.java
+++ src/bnet.java
@@ -1,22 +1,6 @@
 import java.util.ArrayList;
 
-public class bnet {
-
-	//======================================================================
-	static final double PB = 0.001;
-	static final double PE = 0.002;
-	
-	static final double PA_BT_ET = 0.95;
-	static final double PA_BT_EF = 0.94;
-	static final double PA_BF_ET = 0.29;
-	static final double PA_BF_EF = 0.001;
-	
-	static final double PJ_AT = 0.90;
-	static final double PJ_AF = 0.05;
-	
-	static final double PM_AT = 0.70;
-	static final double PM_AF = 0.01;
-	//=====================================================================
+public class Bnet {
 	
 	// B = burglary
 	// E = earthquake
@@ -24,45 +8,194 @@ public class bnet {
 	// J = John calls
 	// M = Mary calls
 	
+	ArrayList<Variable> variables;
+	ArrayList<Variable> evidence;
 	
-	public boolean contains_given;
-	public int given_index;
+	/*boolean burglary;
+	boolean earthquake;
+	boolean alarm;
+	boolean johnCalls;
+	boolean maryCalls;*/
 	
-	public boolean b;
-	public boolean e;
-	public boolean a;
-	public boolean j;
-	public boolean m;
-	public boolean joint;
+	Variable b;
+	Variable e;
+	Variable a;
+	Variable j;
+	Variable m;
 	
-	public bnet() {
-		contains_given = false;
-		given_index = -1;
+	public Bnet() {
+		variables = new ArrayList<Variable>();
+		evidence = new ArrayList<Variable>();
 	}
 	
+	//Given an input such as 'At' splits into variable Alarm and boolean true
+	public static Variable get_var(String[] args, int i) {
+		Variable v = new Variable();
+		v.letter = args[i].charAt(0);
+		
+		if ((v.letter == 'b') || (v.letter == 'B')) {
+			v.type = Variable.Type.INITIAL;
+		} else if ((v.letter == 'e') || (v.letter == 'E')) {
+			v.type = Variable.Type.INITIAL;
+		} else if ((v.letter == 'a') || (v.letter == 'A')) {
+			v.type = Variable.Type.INTERMEDIATE;
+		} else if ((v.letter == 'j') || (v.letter == 'J')) {
+			v.type = Variable.Type.END;
+		} else if ((v.letter == 'm') || (v.letter == 'M')) {
+			v.type = Variable.Type.END;
+		} else {
+			v.type = Variable.Type.NONE;
+		}
+		
+		if (args[i].charAt(1) == 't') {
+			v.bool = true;
+		} else {
+			v.bool = false;
+		}
+		
+		return v;
+	}
+	
+	//Reads our input from command line and separates into query and evidence
 	public void handle_input(String[] args) {
+		boolean contains_given = false;
+		int given_index = -1;
+		
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].equalsIgnoreCase("given")) {
-				this.contains_given = true;
-				this.given_index = i;
+				contains_given = true;
+				given_index = i;
 			}
 		}
 		
+		if (contains_given == true) {
+			for (int i = 0; i < given_index; i++) {
+				Variable v = get_var(args, i);
+				this.variables.add(v);
+			}
+			
+			for (int i = given_index+1; i < args.length; i++) {
+				Variable v = get_var(args, i);
+				this.evidence.add(v);
+			}
+		} else {
+			for (int i = 0; i < args.length; i++) {
+				Variable v = get_var(args, i);
+				this.variables.add(v);
+			}
+		}
 		
 	}
 	
-	public double enumerationAsk(variable x, ArrayList<variable> e) {
-		return 0;
+	//Given a variable, prints information and sets Bnet value apropriately
+	public void handle_variable(Variable v) {
+		if ((v.letter == 'a') || (v.letter == 'A')) {
+			System.out.print("Alarm");
+			if (v.bool == true) {
+				this.a.iBool = 1;
+			} else {
+				this.a.iBool = 0; 
+			}
+		} else if ((v.letter == 'b') || (v.letter == 'B')) {
+			System.out.print("Burglary");
+			if (v.bool == true) {
+				this.b.iBool = 1;
+			} else {
+				this.b.iBool = 0; 
+			}
+		} else if ((v.letter == 'e') || (v.letter == 'E')){
+			System.out.print("Earthquake");
+			if (v.bool == true) {
+				this.e.iBool = 1;
+			} else {
+				this.e.iBool = 0; 
+			}
+		} else if ((v.letter == 'm') || (v.letter == 'M')) {
+			System.out.print("Mary calls");
+			if (v.bool == true) {
+				this.m.iBool = 1;
+			} else {
+				this.m.iBool = 0; 
+			}
+		} else if ((v.letter == 'j') || (v.letter == 'J')) {
+			System.out.print("John calls");
+			if (v.bool == true) {
+				this.j.iBool = 1;
+			} else {
+				this.j.iBool = 0; 
+			}
+		} else {
+			System.out.print("Unknown ");
+		}
+		
+		System.out.print(" is " + v.bool + "\n");
+	}
+	
+	//Prints what we are looking for and what we know
+	public void handle_information() {
+		System.out.println("We are looking for the following information: ");
+		for (int i = 0; i < this.variables.size(); i++) {
+			handle_variable(variables.get(i));
+		}
+		
+		System.out.println("We know the following information: ");
+		for (int i = 0; i < this.evidence.size(); i++) {
+			handle_variable(evidence.get(i));
+		}
+	}
+	
+	public double numerator(Variable b, Variable e, Variable a, Variable j, Variable m) {
+		Variable var = new Variable();
+		
+		double n = 0;
+		int SIZE = 2;
+		for (int v = 0; v < SIZE; v++) { //Earthquake
+			for (int w = 0; w < SIZE; w++) { //Burglary
+				for (int x = 0; x < SIZE; x++) { //Alarm
+					for (int y = 0; y < SIZE; y++) { //John calls
+						for (int z = 0; z < SIZE; z++) { //Mary calls
+							n = var.P(b, var, var) 
+									* var.P(e, var, var) 
+									* var.P(a, b, e) 
+									* var.P(j, a, var) 
+									* var.P(m, a, var);
+						}
+					}
+				}
+			}
+		}
+		
+		return n;
+	}
+	
+	public double denomenator() {
+		Variable var = new Variable();
+		
+		double d = 0;
+		int SIZE = 2;
+		for (int v = 0; v < SIZE; v++) { //Earthquake
+			for (int w = 0; w < SIZE; w++) { //Burglary
+				for (int x = 0; x < SIZE; x++) { //Alarm
+					for (int y = 0; y < SIZE; y++) { //John calls
+						for (int z = 0; z < SIZE; z++) { //Mary calls
+							d = 0;
+						}
+					}
+				}
+			}
+		}
+		
+		return d;
 	}
 	
 	//Returns the joint probability of the five events
-	public boolean computeProbability(boolean b, boolean e, boolean a, boolean j, boolean m) {
+	public double computeProbability(boolean b, boolean e, boolean a, boolean j, boolean m) {
 		
-		return false;
+		return 0;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to our Bayesian Network!");
+		System.out.println("Welcome to our Bayesian Network!=========================");
 		if (args.length == 0) {
 			System.out.println("Please rerun the program with command line arguments");
 			System.exit(0);
@@ -72,11 +205,18 @@ public class bnet {
 			System.exit(0);
 		}
 		
-		bnet b = new bnet();
+		Bnet b = new Bnet();
 		b.handle_input(args);
-		b.computeProbability(b.b, b.e, b.a, b.j, b.m);
+		b.handle_information();
+		double n = b.numerator(b.b, b.e, b.a, b.j, b.m);
+		System.out.println("Numerator value is: " + n);
 		
+		//double d = b.denomenator();
+		//double answer = n/d;
 		
+		//System.out.println("The probability is; " + answer);
+		//b.computeProbability(b.b, b.e, b.a, b.j, b.m);
+	
 	}
 
 }
--- src/variable.java
+++ src/variable.java
@@ -1,17 +1,115 @@
 
-public class variable {
+public class Variable {
 
-	char letter;
-	String name;
-	boolean bool;
-	double value;
+	//======================================================================
+	static final double PB = 0.001;
+	static final double PE = 0.002;
 	
-	public variable() {
+	static final double PA_BT_ET = 0.95;
+	static final double PA_BT_EF = 0.94;
+	static final double PA_BF_ET = 0.29;
+	static final double PA_BF_EF = 0.001;
+	
+	static final double PJ_AT = 0.90;
+	static final double PJ_AF = 0.05;
+	
+	static final double PM_AT = 0.70;
+	static final double PM_AF = 0.01;
+	//=====================================================================
+	
+	public enum Type {
+		INITIAL, INTERMEDIATE, END, NONE
+	}
+	
+	char letter;
+	boolean bool;
+	
+	int iBool;
+	Type type;
+	
+	//Default constructor
+	public Variable() {
 		letter = 'x';
-		name = "error";
-		bool = false;
-		value = 0;
+		boolean bool;
 		
+		int iBool = 0;
+		type = Type.NONE;
+	}
+	
+	public double P(Variable query, Variable p1, Variable p2) {
+		if (type == Type.INITIAL) { //No parents
+			if ((query.letter == 'B') || (query.letter == 'b')) { //If dealing with Burglary
+				if (query.iBool == 1) {
+					return PB;
+				}
+				return (1-PB);
+			} 
+			
+			//Else if dealing with Earthquake
+			if (query.iBool == 1) {
+				return PE;
+			}
+			return (1-PE);
+		} else if (type == Type.INTERMEDIATE) { //Two parents (B = p1, E = p2)
+			if ((query.letter == 'A') || (query.letter == 'a')) {
+				if ((p1.iBool == 1) && (p2.iBool == 1)) { //B = t and E = t
+					if (query.iBool == 1) {
+						return PA_BT_ET;
+					}
+					return (1 - PA_BT_ET);
+				} else if ((p1.iBool == 1) && (p2.iBool == 0)) { //B = t and E = f
+					if (query.iBool == 1) {
+						return PA_BT_EF;
+					}
+					return (1 - PA_BT_EF);
+				} else if ((p1.iBool == 0) && (p2.iBool == 1)) { //B = f and E = t
+					if (query.iBool == 1) {
+						return PA_BF_ET;
+					}
+					return (1 - PA_BF_ET);
+				} else if ((p1.iBool == 0) && (p2.iBool == 0)) { //B = f and E = f
+					if (query.iBool == 1) {
+						return PA_BF_EF;
+					}
+					return (1 - PA_BF_EF);
+				} else {
+					return -1000000;
+				}
+			}
+			return -1000000; //Error has occured! Only intermediate should be Alarm
+		} else if (type == Type.END) { //One parent
+			if ((query.letter == 'J') || (query.letter == 'j')) {
+				if (p1.iBool == 1) { //If A = t
+					if (query.iBool == 1) {
+						return PJ_AT;
+					}
+					return (1 - PJ_AT);
+				} else if (p1.iBool == 0) { //If A = f
+					if (query.iBool == 1) {
+						return PJ_AF;
+					}
+					return (1 - PJ_AF);
+				} else {
+					return -1000000;
+				}
+			} else if ((query.letter == 'M') || (query.letter == 'm')) {
+				if (p1.iBool == 1) { //If A = t
+					if (query.iBool == 1) {
+						return PM_AT;
+					}
+					return (1 - PM_AT);
+				} else if (p1.iBool == 0) { //If A = f
+					if (query.iBool == 1) {
+						return PM_AF;
+					}
+					return (1 - PM_AF);
+				} else {
+					return -1000000;
+				}
+			}
+			return -10000;
+		}
+		return -10000;
 	}
 	
 }
